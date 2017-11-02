6069b895af6af62eac8f9e2898b00c2216f70518
Jean-Francois Moine <moinejf@free.fr>
2017-11-02 17:14:18 +0100

    play: add volume in the play events and handle its null value

--- util/toaudio5.js
+++ util/toaudio5.js
@@ -48,6 +48,7 @@
 //		[2]: MIDI instrument
 //		[3]: MIDI note pitch (with cents)
 //		[4]: duration
+//		[5]: volume (0..1 - optional)
 //
 // stop() - stop playing
 //
@@ -402,7 +403,8 @@ function Audio5(i_conf) {
 		while (1) {
 			o = ac.createBufferSource();
 			o.buffer = sounds[e[2]][e[3] | 0];
-			o.connect(gain);
+			if (e[5] != 0)		// if no sound (rest), don't connect
+				o.connect(gain)
 			if (o.detune) {
 				d = (e[3] * 100) % 100
 				if (d)			// if micro-tone
