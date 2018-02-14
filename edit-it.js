c44f72e534cf5c16e161163dd29a7deabab1d9f2
Jean-Francois Moine <moinejf@free.fr>
2018-02-14 10:54:54 +0100

    translation: add the italian language
    Submitted by Guido Gonzato.

---
+++ edit-it.js
@@ -0,0 +1,48 @@
+//JS - abc2svg edit - text in Italian
+function loadtxt() {
+	texts = {
+		bad_nb: 'Numero di linea errato',
+		fn: 'Nome file: ',
+		load: 'Carica il file da includere ',
+		play: 'Suona',
+		stop: 'Ferma',
+	}
+	var text_kv = [
+		"a", 'A proposito',
+		"b", 'File ABC:',
+//		"df", 'Caratteristiche di abc2svg',
+//		"dp", 'Parametri di abc2svg',
+		"er", 'Errori',
+		"f", 'File',
+		"fl", 'Carica file',
+		"fo", 'Segui la musica',
+		"fs", 'Dimensione del font',
+		"gv", 'Volume',
+		"h", 'Aiuto',
+		"ha", 'Aiuto',
+		"lg", 'Lingua',
+		"playbutton", 'Suona',
+		"pr", 'Preferenze',
+		"saveas", 'Salva il file',
+		"sftl", 'Tipo',
+		"sful", 'URL del Soundfont',
+		"sp", 'Velocità'
+	]
+
+	for (var i = 0; i < text_kv.length; i += 2)
+		document.getElementById(text_kv[i]).innerHTML = text_kv[i + 1];
+
+	document.getElementById("hlp").outerHTML = '<ul id="hlp">\n\
+<li>Opzioni:\n\
+    <ul>\n\
+	<li>scrivere direttamente codice ABC nell'area di testo, oppure</li>\n\
+	<li>incollare codice ABC nell'area di testo, oppure</li>\n\
+	<li>caricare un file ABC locale (menu \'ABC | Carica file\'), oppure</li>\n\
+	<li>trascinare un file locale da file manager\n\
+		o del testo selezionato nell'area di testo.</li>\n\
+    </ul></li>\n\
+	<li>Puoi modificare il codice ABC nell'area di testo;<br/>\n\
+	dopo 2 secondi la musica viene visualizzata o aggiornata.</li>\n\
+	<li>L'opzione \'Stampa\' del browser stampa la musica.</li>\n\
+</ul>'
+}
---
+++ err-it.js
@@ -0,0 +1,154 @@
+//JS abc2svg - Italian translation of the error codes
+//	Traduzione dei codici di errore in italiano
+//	di Guido Gonzato, guido . gonzato at gmail . com
+// Richiamare questo file da linea di comando per avere 
+// i messaggi di errore in italiano.
+
+user.textrans = {
+	"+: lyric without music": "+: parole senza musica",
+	"+: symbol line without music":
+			"+: linea di simboli senza musica",
+	"+: without previous info field": "Nessun campo di informazioni prima di +:",
+	"'{' in grace note": "'{' in nota di abbellimento",
+	"'}', ')' or ']' missing in %%staves":
+				"'}', ')' o ']' mancanti in %%staves",
+	"',' lacking in annotation '@x,y'":
+				"Manca una ',' nell'annotazione '@x,y'",
+	"'$1' is not a note": "'$1' non è una nota",
+	"'$1:' line ignored": "'$1:' linea ignorata",
+	"$1: inside tune - ignored": "$1: nel corpo del brano - ignorato",
+	"!$1! must be on a note": "!$1! deve stare su di una nota",
+	"!$1! must be on a note or a rest":
+				"!$1! deve stare su una nota o su una pausa",
+	"!$1! must be on the last of a couple of notes":
+				"!$1! deve stare sulla seconda di due note",
+	"!beamon! must be on a bar": "!beamon! deve stare su una stanghetta",
+	"!xstem! must be on a note": "!xstem! deve stare su una nota",
+	"%%deco: abnormal h/wl/wr value '$1'":
+				"%%deco: valore '$1' illegale per h/wl/wr",
+	"%%deco: bad C function index '$1'":
+				"%%deco: indice errato per la funzione C '$1'",
+	"%%deco: bad C function value '$1'":
+				"%%deco: valore errato per la funzione C '$1'",
+	"%%deco: cannot have a negative value '$1'":
+				"%%deco: '$1' non può avere un valore negativo",
+	"%%multicol end without start": "%%multicol end senza start",
+	"%%multicol new without start": "%%multicol new senza start",
+	"%%repeat cannot start a tune": "%%repeat non può stare all'inizio del brano",
+	"%%select ignored": "%%select ignorato",
+	"%%staffwidth too big": "%%staffwidth troppo grande",
+	"%%staffwidth too small": "%%staffwidth troppo piccolo",
+	"%%tune not treated yet": "%%tune non ancora gestito",
+	"%%voice ignored": "%%voice ignorato",
+	"%%vskip cannot be negative": "%%vskip non può essere negativo",
+	"Bad !xstem!": "!xstem! errato",
+	"Bad %%setbarnb value": "Valore errato in %%setbarnb",
+	"Bad %%staff number $1 (cur $2, max $3)":
+				"Numero $1 in %%staff errato (attuale $2, max $3)",
+	"Bad %%staff value '$1'": "Valore errato '$1' in %%staff",
+	"Bad %%stafflines value": "Valore errato in %%stafflines",
+	"Bad %%staffscale value": "Valore errato in %%staffscale",
+	"Bad char '$1' in M:": "Carattere errato '$1' in M:",
+	"Bad character '$1'": "Carattere errato '$1'",
+	"Bad controller number in %%MIDI": "Numero errato del controller in %%MIDI",
+	"Bad controller value in %%MIDI": "Valore errato del controller in %%MIDI",
+	"Bad duration '$1' in M:": "Durata '$1' errata in M:",
+	"Bad integer value": "Valore intero errato",
+	"Bad instr= value": "Valore errato di instr=",
+	"Bad L: value": "Valore errato per L:",
+	"Bad note in %%map": "Nota errata in %%map",
+	"Bad page width": "Larghezza di pagina errata",
+	"Bad program in %%MIDI": "Numero di programma non valido in %%MIDI",
+	"Bad scale value in %%font": "Valore di scala non valido in %%font",
+	"Bad tie": "Legatura non valida",
+	"Bad transpose value": "Valore non valido per la trasposizione",
+	"Bad user character '$1'": "Carattere illegale '$1' in U:/%%user",
+	"Bad value '$1' in %%linebreak - ignored":
+				"Valore errato '$1' in %%linebreak - ignorato",
+	"Bad value in $1": "Valore errato in $1",
+	"Bad voice ID in %%staves": "ID di voce non valido in %%staves",
+	"Bar in repeat sequence": "Stanghetta in sequenza ripetuta",
+	"Cannot have !$1! on a head": "!$1! non è consentito su una nota",
+	"Cannot have %%fullsvg inside a tune":
+				"'%%fullsvg' non è consentito entro il brano",
+	"Cannot have a bar in grace notes":
+			"Stanghette non consentite nelle note di abbellimento",
+	"Cannot have a broken rhythm in grace notes":
+			"'Broken rhythm' non consentito nelle note di abbellimento",
+	"Cannot have V:* in tune body":
+			"V:* non consentito nel corpo del brano",
+	"Cannot read file '$1'": "File '$1' inesistente",
+	"Decoration ignored": "Decorazione ignorata",
+	"Empty line in tune header - ignored":
+				"Linea vuota nella testata - ignorata",
+	"Erroneous end of voice overlap": "Fine di voice overlay errata",
+	"Incorrect 1st value in %%repeat": "Primo valore in %%repeat errato",
+	"Incorrect 2nd value in %%repeat": "Secondo valore in %%repeat errato",
+	"Invalid 'r' in tuplet": "'r' non valido in gruppo irregolare",
+	"Invalid decoration '$1'": "Decorazione '$1' non valida",
+	"Invalid note duration $1": "Durée de note $1 non valida",
+	"Lack of ']'": "']' mancante",
+	"Lack of ending $1 in U:/%%user": "Manca $1 alla fine di U:/%%user",
+	'Lack of starting ! or " in U: / %%user':
+				'U: / %%user non inizia con ! o "',
+	"Line split problem - adjust maxshrink and/or breaklimit":
+		"Problema di linea divisa - modificare maxshrink e/o breaklimit",
+	"Line too much shrunk $1 $2 $3": "Linea troppo compressa $1 $2 $3",
+	"Line underfull ($1pt of $2pt)": "Linea troppo vuota ($1pt di $2pt)",
+	"Misplaced '$1' in %%staves": "'$1' in posizione errata in %%staves",
+	"Misplaced dot": "Punto in posizione errata",
+	"Mix of old and new transposition syntaxes":
+			"Sintassi di trasposizione vecchia e nuova allo stesso tempo",
+	"No $1 after %%$2": "Manca $1 dopo %%$2",
+	"No </defs> in %%beginsvg sequence":
+				"Manca </defs> nella sequenza %%beginsvg",
+	"No </style> in %%beginsvg sequence":
+				"manca </style> nella sequenza %%beginsvg",
+	"No accidental after 'exp'": "Manca l'alterazione dopo 'exp'",
+	"No end of decoration": "Decorazione non terminata",
+	"No end of grace note sequence": "Sequenza di note di abbellimento non terminata",
+	"No end of guitar chord": "Accordo di accompagnamento non terminato",
+	"No end of repeat string": "Stringa di ripetizione non terminata",
+	"No end of tuplet": "Gruppo irregolare non terminato",
+	"No end of tuplet in this music line":
+				"Gruppo irregolare non terminato in questa linea",
+	"No function for decoration '$1'": "Funzione inesistente per la decorazione '$1'",
+	"No note before '-'": "Nota mancante davanti a '-'",
+	"No note before '<'": "Nota mancante davanti a '<'",
+	"No note in voice overlay": "Nota mancante in voce sovrapposta (overlay)",
+	"No read_file support": "La funzione read_file non è supportata",
+	"No start of !$1!": "Manca l'inizio di !$1!",
+	"Not a note in grace note sequence":
+				"Carattere errato nelle note di abbellimento",
+	"Not an ASCII character": "Non è un carattere ASCII",
+	"Not enough measure bars for lyric line":
+				"Battute insufficienti per le parole",
+	"Not enough measure bars for symbol line":
+				"Battute insufficienti per la linea s:",
+	"Not enough measures for %%repeat":
+				"Battute insufficienti per %%repeat",
+	"Not enough parameters in %%map":
+				"Parametri insufficienti in %%map",
+	"Not enough notes/rests for %%repeat":
+				 "Note o pause insufficienti in %%repeat",
+	"Note too long": "Durata della nota troppo lunga",
+	"Note too short": "Durata della nota troppo corta",
+	"s: without music": "s: senza musica",
+	"Too many elements in symbol line": "Troppi elementi nella linea s:",
+	"Too many include levels": "Troppi livelli di inclusione di file",
+	"Too many ties": "Troppi simboli di legato",
+	"Too many words in lyric line": "Troppe parole per la linea di musica",
+	"Unknown bagpipe-like key": "Chiave per cornamusa sconosciuta",
+	"Unknown clef '$1'": "Chiave '$1' sconosciuta",
+	"Unknown decoration '$1'": "Decorazione '$1' sconosciuta",
+	"Unknown font $1": "Font $1 sconosciuto",
+	"Unknown glyph: '$1'": "Glifo '$1' sconosciuto",
+	"Unknown keyword '$1' in %%multicol": "Keyword '$1' sconosciuta in %%multicol",
+        "Unsecure code": "Codice pericoloso",
+        "Unterminated string": "Stringa di testo non terminata",
+	"Unterminated string in Q:": "Stringa di testo non terminata in Q:",
+	"Voice overlay already started": "Voce sovrapposta (overlay) già iniziata",
+	"w: without music": "w: senza musica",
+	"Wrong duration in voice overlay":
+				"Durata errata nella voce sovrapposta (overlay)",
+}
