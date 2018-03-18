1ccf4f5786691e2d11636d0697c87da753695249
Jean-Francois Moine <moinejf@free.fr>
2018-03-18 19:22:27 +0100

    util: play: cleanup the code of the sf2-parser

--- util/sf2-parser.js
+++ util/sf2-parser.js
@@ -9,6 +9,9 @@
  * Based on code from the "SoundFont Synthesizer for WebMidiLink"
  *   https://github.com/gree/sf2synth.js
  *
+ * Adapted to abc2svg
+ * Copyright (C) 2018 Jean-Francois Moine
+ *
  * Licensed under the MIT License.
  */
 
@@ -64,20 +67,18 @@
 
     sf2.Parser.prototype.parse = function () {
       /** @type {sf2.Riff.Parser} */
-      var parser = new sf2.Riff.Parser(this.input, this.parserOptions);
+      var parser = new sf2.Riff.Parser(this.input, this.parserOptions),
       /** @type {?sf2.Riff.Chunk} */
-      var chunk;
+	  chunk;
 
       // parse RIFF chunk
       parser.parse();
-      if (parser.chunkList.length !== 1) {
+      if (parser.chunkList.length !== 1)
         throw new Error('wrong chunk length');
-      }
 
       chunk = parser.getChunk(0);
-      if (chunk === null) {
+      if (chunk === null)
         throw new Error('chunk not found');
-      }
 
       this.parseRiffChunk(chunk);
 
@@ -93,40 +94,37 @@
      */
     sf2.Parser.prototype.parseRiffChunk = function (chunk) {
       /** @type {sf2.Riff.Parser} */
-      var parser;
+      var parser,
       /** @type {ByteArray} */
-      var data = this.input;
+	  data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {string} */
-      var signature;
+	  signature;
 
       // check parse target
-      if (chunk.type !== 'RIFF') {
+      if (chunk.type !== 'RIFF')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       // check signature
       signature = String.fromCharCode(data[ip++], data[ip++], data[ip++], data[ip++]);
-      if (signature !== 'sfbk') {
+      if (signature !== 'sfbk')
         throw new Error('invalid signature:' + signature);
-      }
 
       // read structure
       parser = new sf2.Riff.Parser(data, {'index': ip, 'length': chunk.size - 4});
       parser.parse();
-      if (parser.getNumberOfChunks() !== 3) {
+      if (parser.getNumberOfChunks() !== 3)
         throw new Error('invalid sfbk structure');
-      }
 
       // INFO-list
-      this.parseInfoList(/** @type {!sf2.Riff.Chunk} */(parser.getChunk(0)));
+      this.parseInfoList(/** @type {!sf2.Riff.Chunk} */parser.getChunk(0));
 
       // sdta-list
-      this.parseSdtaList(/** @type {!sf2.Riff.Chunk} */(parser.getChunk(1)));
+      this.parseSdtaList(/** @type {!sf2.Riff.Chunk} */parser.getChunk(1));
 
       // pdta-list
-      this.parsePdtaList(/** @type {!sf2.Riff.Chunk} */(parser.getChunk(2)));
+      this.parsePdtaList(/** @type {!sf2.Riff.Chunk} */parser.getChunk(2));
     };
 
     /**
@@ -134,24 +132,22 @@
      */
     sf2.Parser.prototype.parseInfoList = function (chunk) {
       /** @type {sf2.Riff.Parser} */
-      var parser;
+      var parser,
       /** @type {ByteArray} */
-      var data = this.input;
+	  data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {string} */
-      var signature;
+	  signature;
 
       // check parse target
-      if (chunk.type !== 'LIST') {
+      if (chunk.type !== 'LIST')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       // check signature
       signature = String.fromCharCode(data[ip++], data[ip++], data[ip++], data[ip++]);
-      if (signature !== 'INFO') {
+      if (signature !== 'INFO')
         throw new Error('invalid signature:' + signature);
-      }
 
       // read structure
       parser = new sf2.Riff.Parser(data, {'index': ip, 'length': chunk.size - 4});
@@ -163,34 +159,31 @@
      */
     sf2.Parser.prototype.parseSdtaList = function (chunk) {
       /** @type {sf2.Riff.Parser} */
-      var parser;
+      var parser,
       /** @type {ByteArray} */
-      var data = this.input;
+	  data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {string} */
-      var signature;
+	  signature;
 
       // check parse target
-      if (chunk.type !== 'LIST') {
+      if (chunk.type !== 'LIST')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       // check signature
       signature = String.fromCharCode(data[ip++], data[ip++], data[ip++], data[ip++]);
-      if (signature !== 'sdta') {
+      if (signature !== 'sdta')
         throw new Error('invalid signature:' + signature);
-      }
 
       // read structure
       parser = new sf2.Riff.Parser(data, {'index': ip, 'length': chunk.size - 4});
       parser.parse();
-      if (parser.chunkList.length !== 1) {
+      if (parser.chunkList.length !== 1)
         throw new Error('TODO');
-      }
       this.samplingData =
         /** @type {{type: string, size: number, offset: number}} */
-        (parser.getChunk(0));
+	  parser.getChunk(0);
     };
 
     /**
@@ -198,33 +191,30 @@
      */
     sf2.Parser.prototype.parsePdtaList = function (chunk) {
       /** @type {sf2.Riff.Parser} */
-      var parser;
+      var parser,
       /** @type {ByteArray} */
-      var data = this.input;
+	  data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {string} */
-      var signature;
+	  signature;
 
       // check parse target
-      if (chunk.type !== 'LIST') {
+      if (chunk.type !== 'LIST')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       // check signature
       signature = String.fromCharCode(data[ip++], data[ip++], data[ip++], data[ip++]);
-      if (signature !== 'pdta') {
+      if (signature !== 'pdta')
         throw new Error('invalid signature:' + signature);
-      }
 
       // read structure
       parser = new sf2.Riff.Parser(data, {'index': ip, 'length': chunk.size - 4});
       parser.parse();
 
       // check number of chunks
-      if (parser.getNumberOfChunks() !== 9) {
+      if (parser.getNumberOfChunks() !== 9)
         throw new Error('invalid pdta chunk');
-      }
 
       this.parsePhdr(/** @type {sf2.Riff.Chunk} */(parser.getChunk(0)));
       this.parsePbag(/** @type {sf2.Riff.Chunk} */(parser.getChunk(1)));
@@ -242,18 +232,17 @@
      */
     sf2.Parser.prototype.parsePhdr = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {Array.<Object>} */
-      var presetHeader = this.presetHeader = [];
+	  presetHeader = this.presetHeader = [],
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size;
 
       // check parse target
-      if (chunk.type !== 'phdr') {
+      if (chunk.type !== 'phdr')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       while (ip < size) {
         presetHeader.push({
@@ -273,18 +262,17 @@
      */
     sf2.Parser.prototype.parsePbag = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {Array.<Object>} */
-      var presetZone = this.presetZone = [];
+	  presetZone = this.presetZone = [],
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size;
 
       // check parse target
-      if (chunk.type !== 'pbag') {
+      if (chunk.type !== 'pbag')
         throw new Error('invalid chunk type:'  + chunk.type);
-      }
 
       while (ip < size) {
         presetZone.push({
@@ -299,9 +287,8 @@
      */
     sf2.Parser.prototype.parsePmod = function (chunk) {
       // check parse target
-      if (chunk.type !== 'pmod') {
+      if (chunk.type !== 'pmod')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       this.presetZoneModulator = this.parseModulator(chunk);
     };
@@ -311,9 +298,8 @@
      */
     sf2.Parser.prototype.parsePgen = function (chunk) {
       // check parse target
-      if (chunk.type !== 'pgen') {
+      if (chunk.type !== 'pgen')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
       this.presetZoneGenerator = this.parseGenerator(chunk);
     };
 
@@ -322,18 +308,17 @@
      */
     sf2.Parser.prototype.parseInst = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {Array.<Object>} */
-      var instrument = this.instrument = [];
+	  instrument = this.instrument = [],
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size;
 
       // check parse target
-      if (chunk.type !== 'inst') {
+      if (chunk.type !== 'inst')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       while (ip < size) {
         instrument.push({
@@ -348,19 +333,17 @@
      */
     sf2.Parser.prototype.parseIbag = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {Array.<Object>} */
-      var instrumentZone = this.instrumentZone = [];
+	  instrumentZone = this.instrumentZone = [],
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size;
 
       // check parse target
-      if (chunk.type !== 'ibag') {
+      if (chunk.type !== 'ibag')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
-
 
       while (ip < size) {
         instrumentZone.push({
@@ -375,9 +358,8 @@
      */
     sf2.Parser.prototype.parseImod = function (chunk) {
       // check parse target
-      if (chunk.type !== 'imod') {
+      if (chunk.type !== 'imod')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       this.instrumentZoneModulator = this.parseModulator(chunk);
     };
@@ -388,9 +370,8 @@
      */
     sf2.Parser.prototype.parseIgen = function (chunk) {
       // check parse target
-      if (chunk.type !== 'igen') {
+      if (chunk.type !== 'igen')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       this.instrumentZoneGenerator = this.parseGenerator(chunk);
     };
@@ -400,58 +381,52 @@
      */
     sf2.Parser.prototype.parseShdr = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {Array.<Object>} */
-      var samples = this.sample = [];
+	  samples = this.sample = [],
       /** @type {Array.<Object>} */
-      var sampleHeader = this.sampleHeader = [];
+	  sampleHeader = this.sampleHeader = [],
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size,
       /** @type {string} */
-      var sampleName;
+	  sampleName,
       /** @type {number} */
-      var start;
+	  start,
       /** @type {number} */
-      var end;
+	  end,
       /** @type {number} */
-      var startLoop;
+	  startLoop,
       /** @type {number} */
-      var endLoop;
+	  endLoop,
       /** @type {number} */
-      var sampleRate;
+	  sampleRate,
       /** @type {number} */
-      var originalPitch;
+	  originalPitch,
       /** @type {number} */
-      var pitchCorrection;
+	  pitchCorrection,
       /** @type {number} */
-      var sampleLink;
+	  sampleLink,
       /** @type {number} */
-      var sampleType;
+	  sampleType;
 
       // check parse target
-      if (chunk.type !== 'shdr') {
+      if (chunk.type !== 'shdr')
         throw new Error('invalid chunk type:' + chunk.type);
-      }
 
       while (ip < size) {
         sampleName = String.fromCharCode.apply(null, data.subarray(ip, ip += 20));
-        start = (
-          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24)
-        ) >>> 0;
-        end = (
-          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24)
-        ) >>> 0;
-        startLoop = (
-          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24)
-        ) >>> 0;
-        endLoop =  (
-          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24)
-        ) >>> 0;
-        sampleRate = (
-          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24)
-        ) >>> 0;
+        start =
+          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24);
+        end =
+          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24);
+        startLoop =
+          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24);
+        endLoop =
+          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24);
+        sampleRate =
+          (data[ip++] << 0) | (data[ip++] << 8) | (data[ip++] << 16) | (data[ip++] << 24);
         originalPitch = data[ip++];
         pitchCorrection = (data[ip++] << 24) >> 24;
         sampleLink = data[ip++] | (data[ip++] << 8);
@@ -497,15 +472,15 @@
     // and always forces a sample rate of 22050 or higher. Why?
     sf2.Parser.prototype.adjustSampleData = function (sample, sampleRate) {
       /** @type {Int16Array} */
-      var newSample;
+      var newSample,
       /** @type {number} */
-      var i;
+	  i,
       /** @type {number} */
-      var il;
+	  il,
       /** @type {number} */
-      var j;
+	  j,
       /** @type {number} */
-      var multiply = 1;
+	  multiply = 1;
 
       // buffer
       while (sampleRate < 22050) {
@@ -531,17 +506,17 @@
      */
     sf2.Parser.prototype.parseModulator = function (chunk) {
         /** @type {ByteArray} */
-        var data = this.input;
+        var data = this.input,
         /** @type {number} */
-        var ip = chunk.offset;
+	    ip = chunk.offset,
         /** @type {number} */
-        var size = chunk.offset + chunk.size;
+	    size = chunk.offset + chunk.size,
         /** @type {number} */
-        var code;
+	    code,
         /** @type {string} */
-        var key;
+	    key,
         /** @type {Array.<Object>} */
-        var output = [];
+	    output = [];
 
         while (ip < size) {
           // Src  Oper
@@ -606,17 +581,17 @@
      */
     sf2.Parser.prototype.parseGenerator = function (chunk) {
       /** @type {ByteArray} */
-      var data = this.input;
+      var data = this.input,
       /** @type {number} */
-      var ip = chunk.offset;
+	  ip = chunk.offset,
       /** @type {number} */
-      var size = chunk.offset + chunk.size;
+	  size = chunk.offset + chunk.size,
       /** @type {number} */
-      var code;
+	  code,
       /** @type {string} */
-      var key;
+	  key,
       /** @type {Array.<Object>} */
-      var output = [];
+	  output = [];
 
       while (ip < size) {
         code = data[ip++] | (data[ip++] << 8);
@@ -663,29 +638,29 @@
 
     sf2.Parser.prototype.getInstruments = function () {
       /** @type {Array.<Object>} */
-      var instrument = this.instrument;
+      var instrument = this.instrument,
       /** @type {Array.<Object>} */
-      var zone = this.instrumentZone;
+	  zone = this.instrumentZone,
       /** @type {Array.<Object>} */
-      var output = [];
+	  output = [],
       /** @type {number} */
-      var bagIndex;
+	  bagIndex,
       /** @type {number} */
-      var bagIndexEnd;
+	  bagIndexEnd,
       /** @type {Array.<Object>} */
-      var zoneInfo;
+	  zoneInfo,
       /** @type {{generator: Object, generatorInfo: Array.<Object>}} */
-      var instrumentGenerator;
+	  instrumentGenerator,
       /** @type {{modulator: Object, modulatorInfo: Array.<Object>}} */
-      var instrumentModulator;
+	  instrumentModulator,
       /** @type {number} */
-      var i;
+	  i,
       /** @type {number} */
-      var il;
+	  il,
       /** @type {number} */
-      var j;
+	  j,
       /** @type {number} */
-      var jl;
+	  jl;
 
       // instrument -> instrument bag -> generator / modulator
       for (i = 0, il = instrument.length; i < il; ++i) {
@@ -769,20 +744,19 @@
         }
       }; // TODO
       /** @type {Object} */
-      var info;
+      var info,
       /** @type {number} */
-      var i;
+	  i,
       /** @type {number} */
-      var il;
+	  il;
 
       for (i = indexStart, il = indexEnd; i < il; ++i) {
         info = zoneModGen[i];
 
-        if (info.type === 'unknown') {
+        if (info.type === 'unknown')
           modgen.unknown.push(info.value);
-        } else {
+	else
           modgen[info.type] = info.value;
-        }
       }
 
       return {
@@ -857,7 +831,6 @@
       'overridingRootKey'
     ];
 
-
     sf2.Riff = {};
 
     sf2.Riff.Parser = function (input, options) {
@@ -899,26 +872,25 @@
 
       this.chunkList = [];
 
-      while (this.ip < length) {
+      while (this.ip < length)
         this.parseChunk();
-      }
     };
 
     sf2.Riff.Parser.prototype.parseChunk = function () {
       /** @type {ByteArray} */
-      var input = this.input;
+      var input = this.input,
       /** @type {number} */
-      var ip = this.ip;
+	  ip = this.ip,
       /** @type {number} */
-      var size;
+	  size;
 
       this.chunkList.push(new sf2.Riff.Chunk(
         String.fromCharCode(input[ip++], input[ip++], input[ip++], input[ip++]),
         (size = this.bigEndian ?
            ((input[ip++] << 24) | (input[ip++] << 16) |
-            (input[ip++] <<  8) | (input[ip++]      )) >>> 0 :
+            (input[ip++] <<  8) | (input[ip++]      )) :
            ((input[ip++]      ) | (input[ip++] <<  8) |
-            (input[ip++] << 16) | (input[ip++] << 24)) >>> 0
+            (input[ip++] << 16) | (input[ip++] << 24))
         ),
         ip
       ));
@@ -926,9 +898,8 @@
       ip += size;
 
       // padding
-      if (this.padding && ((ip - this.offset) & 1) === 1) {
+      if ((this.padding && (ip - this.offset) & 1) === 1)
         ip++;
-      }
 
       this.ip = ip;
     };
@@ -941,9 +912,8 @@
       /** @type {sf2.Riff.Chunk} */
       var chunk = this.chunkList[index];
 
-      if (chunk === undefined) {
+      if (chunk === undefined)
         return null;
-      }
 
       return chunk;
     };
@@ -954,7 +924,6 @@
     sf2.Riff.Parser.prototype.getNumberOfChunks = function () {
       return this.chunkList.length;
     };
-
 
     return sf2;
 }));
