239ca71bbdd8472529c318737311f3c176738e00
Jean-Francois Moine <moinejf@free.fr>
2018-03-26 10:31:55 +0200

    core: modules: handle %%MIDI drummap

--- modules/MIDI.js
+++ modules/MIDI.js
@@ -8,9 +8,46 @@
 //	%%MIDI channel n
 //	%%MIDI program n
 //	%%MIDI control k v
+//	%%MIDI drummap ABC_note MIDI_pitch
 
 function MIDI(i_abc) {
-    var	abc = i_abc
+    var	abc = i_abc,
+	pits = new Int8Array([0, 0, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6]),
+	accs = new Int8Array([0, 1, 0, -1, 0, 0, 1, 0, -1, 0, -1, 0])
+
+    // convert a MIDI pitch to a note
+    function tonote(p) {
+    var	pit = Number(p)
+	if (isNaN(pit))
+		return
+	p = ((pit / 12) | 0) * 7 - 19;	// octave
+	pit = pit % 12;			// in octave
+	p += pits[pit]
+	note = {
+		pit: p,
+		apit: p,
+	}
+	if (accs[pit])
+		note.acc = accs[pit]
+	return note
+    }
+
+    // normalize a note for mapping
+    function norm(p) {
+    var	a = p.match(/([_^]*)([A-Ga-g])([,']*)/)	// '
+	if (!a) {
+		abc.syntax()
+		return
+	}
+	if (p.match(/[A-Z]/)) {
+		p = p.toLowerCase();
+		if (p.indexOf("'") > 0)
+			p = p.replace("'", '')
+		else
+			p += ','
+	}
+	return p
+    }
 
     // parse %%MIDI commands
     MIDI.prototype.do_midi = function(parm) {
@@ -22,6 +59,21 @@ function MIDI(i_abc) {
 		if (a[2] != "10")
 			break
 		abc.set_v_param("midictl", "0 1") // channel 10 is bank 128 program 0
+		break
+	case "drummap":
+		if (abc.get_cfmt("sound") != "play")
+			break
+//fixme: should have a 'MIDIdrum' per voice?
+		n = norm(a[2]);
+		v = tonote(a[3]);
+		if (!n || !v) {
+			abc.syntax(1, err_bad_val_s, "%%MIDI drummap")
+			break
+		}
+		if (!abc.maps.MIDIdrum)
+			abc.maps.MIDIdrum = {}
+		abc.maps.MIDIdrum[n] = [null, v];
+		abc.set_v_param("mididrum", "MIDIdrum")
 		break
 	case "program":
 		if (a[3] != undefined)	// the channel is unused
@@ -67,6 +119,11 @@ function MIDI(i_abc) {
 			item = a[i + 1].split(' ');
 			curvoice.midictl[item[0]] = Number(item[1])
 			break
+		case "mididrum=":		// %%MIDI drummap note midipitch
+			if (!curvoice.map)
+				curvoice.map = {}
+			curvoice.map = a[i + 1]
+			break
 		}
 	}
     } // set_midi()
@@ -76,7 +133,9 @@ function MIDI(i_abc) {
 	//export some functions/variables
 	abc.tosvg('MIDI', '\
 %%beginjs\n\
+Abc.prototype.get_cfmt = function(k) { return cfmt[k] }\n\
 Abc.prototype.get_curvoice = function() { return curvoice }\n\
+Abc.prototype.maps = maps\n\
 Abc.prototype.set_v_param = set_v_param\n\
 Abc.prototype.syntax = syntax\n\
 \
@@ -97,4 +156,4 @@ set_vp = function(a) {\n\
 %%endjs\n\
 ')
 
-} //Grid()
+} //MIDI()
